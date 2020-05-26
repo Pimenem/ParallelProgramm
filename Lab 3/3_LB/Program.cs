8aff7f9837b250f3d027f9150fadfd3a85594e59
Pimenem <65821641+Pimenem@users.noreply.github.com>
2020-05-26 21:29:18 +0300

    Add files via upload

---
+++ Lab 3/3_LB/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace _3_LB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokenSource = new CancellationTokenSource();
+            var tokenSr = tokenSource.Token;
+
+            Task task = new Task(() => Tok(tokenSr));
+            task.Start();
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Нажмите Enter, чтобы прервать задачу...");
+            Console.ReadKey();
+            tokenSource.Cancel();
+            Console.ReadLine();
+        }
+
+        static void Tok(CancellationToken tokenSr)
+        {
+            while (!tokenSr.IsCancellationRequested) ;
+            Console.WriteLine("Прерывание прошло успешно!");
+
+        }
+    }
+    
+}
